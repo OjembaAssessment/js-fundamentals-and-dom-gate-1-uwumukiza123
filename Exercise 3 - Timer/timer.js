09840d8ce6cb3f11483547453ee4f6c8a15381a5
Laetitia UWUMUKIZA <uwumukizalaetitia@gmail.com>
2023-07-24 10:47:56 +0200

    js & the dom test

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,29 @@
-console.log("Exercise 3 - Timer");
+
+// selecting element
+
+const minus = document.querySelector('.minus');
+const plus = document.querySelector('.plus');
+let counter = document.querySelector('.counter');
+const reset = document.querySelector('.reset');
+const start = document.querySelector('.start')
+
+let count = 0;
+
+plus.addEventListener("click", ()=> {
+    counter.innerHTML = count;
+    count ++;
+});
+
+minus.addEventListener("click", ()=> {
+    counter.innerHTML = count;
+    count --;
+});
+
+reset.addEventListener('click', ()=>{
+    counter.innerHTML = count;
+    count = 0;
+});
+
+start.addEventListener('click', ()=> {
+    start.innerHTML = "Stop"
+})
